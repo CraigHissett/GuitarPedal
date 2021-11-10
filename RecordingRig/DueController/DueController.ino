7251979223dda13aac333d4fece9c0cb26cadbe4
Craig Hissett <craighissett@gmail.com>
2021-11-10 16:17:08 +0000

    Test

--- RecordingRig/DueController/DueController.ino
+++ RecordingRig/DueController/DueController.ino
@@ -1,7 +1,17 @@
 #include <Control_Surface.h>
+#include <LiquidCrystal.h>
 
 // Instantiate a MIDI Interface to use
 USBMIDI_Interface midi;
+
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+Bank<2> bank(4); // A bank with four channels, and 2 bank settings
 
 //latching switch variables
 const uint8_t velocity = 0b1111111; // Maximum velocity (0b1111111 = 0x7F = 127)
@@ -20,32 +30,29 @@ CD74HC4051 mux {
 // Create a new instance of the class 'DigitalLatch', called 'toggleSwitch', on pin 2, that sends MIDI messages with note 'C4' (60) on channel 1, with velocity 127 and a duration of 100ms
 //DigitalLatch toggleSwitch(2, C4, 1, velocity, duration);
 
-// Instantiate a CCButton object
-CCButton button {
-  // Push button on pin 5:
-  5,
-  // General Purpose Controller #1 on MIDI channel 1:
-  {MIDI_CC::General_Purpose_Controller_1, CHANNEL_1},
+Bankable::CCPotentiometer potentiometer1 {
+  {bank, BankType::CHANGE_CHANNEL},     // bank configuration
+  A0,                                   // analog pin
+  {MIDI_CC::Channel_Volume, CHANNEL_1}, // address
 };
 
-// Instantiate an array of CCPotentiometer objects
-CCPotentiometer potentiometers[] {
-  {A0,        // Analog pin connected to potentiometer 1
-   0x10},     // Controller number of the first potentiometer
-  {A1,        // Analog pin connected to potentiometer 2
-   0x11},     // Controller number of the second potentiometer
-  {A2, 0x12}, // Etc.
-  {A3, 0x13},
-  {A4, 0x14},
-  {A5, 0x15},
-};
+
+
+SwitchSelector selector {bank, 11};
 
 // Initialize the Control Surface
 void setup() {
+    // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("CraigMIDI");
+  
   Control_Surface.begin();
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // Update the Control Surface
 void loop() {
   Control_Surface.loop();
+  digitalWrite(LED_BUILTIN, digitalRead(11));
 }
